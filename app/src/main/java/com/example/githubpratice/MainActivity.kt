96a553cee09f0465c0e073c0cc75d871e627f8fa
samgashyeong <samgashyeong@naver.com>
2021-07-09 22:36:13 +0900

    1커밋

--- app/src/main/java/com/example/githubpratice/MainActivity.kt
+++ app/src/main/java/com/example/githubpratice/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.githubpratice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //준상선배 레포지토리임.
+        println("밥먹고싶다.")
     }
 }
