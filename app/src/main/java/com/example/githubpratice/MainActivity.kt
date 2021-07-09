50ee6a95753aba0159304c270ca81853511e29ef
samgashyeong <samgashyeong@naver.com>
2021-07-09 22:28:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/githubpratice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubpratice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //준상선배 레포지토리임.
+    }
+}
